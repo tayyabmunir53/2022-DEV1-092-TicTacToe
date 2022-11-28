e4e8067b4a3046c186d9fa97b67d1f54fd41f9d2
Tayyab Munir <muhammad.munir@ericsson.com>
2022-11-28 16:41:32 +0100

    adds basic Http testcases

--- src/test/java/com/tictaktoe/demo/DemoApplicationTests.java
+++ src/test/java/com/tictaktoe/demo/DemoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.tictaktoe.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
-class DemoApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class DemoApplicationTests {
+
+	@LocalServerPort
+	private int port = 8080;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	public void testPlayTurnAPI() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/start",
+				String.class)).contains("Welcome to tik tac toe, X will take first turn");
+	}
+
+	@Test
+	public void testStartAPI() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/playturn",
+				String.class)).contains("{\"outputBoard\":\"----------------%%%%%%%%%%%%%%%---------------\",\"responseCode\":\"00\",\"responseDescription\":\"\"}");
 	}
 
 }
